5720877305a7132857d4c1a9e0983b57f578125c
Nicolae Farcas <c_nfarcas@groupon.com>
2018-04-16 12:21:32 +0300

    First commit with the class

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pyHive
-Coinhive API support in python
+- pyHive is a python3 implementation of the PHP CoinHive API class
+
+###Usage
+
+```python
+coinhive = CoinHiveAPI("YOUR_SECRET_KEY")
+stats = coinhive.get("/stats/site")
+print(stats.get("history"))
+```
+
+### Donations
+![[Paypal Donations](paypal.me/xafnir ](https://qph.fs.quoracdn.net/main-qimg-7feb81f45db5bd355683bc19b3c7ee00-c "[Paypal Donations](paypal.me/xafnir ")
---
+++ pyHive.py
@@ -0,0 +1,32 @@
+from urllib import parse
+import requests
+import json
+
+"""
+This is a python implementation of the CoinHiveApi class provided by CoinHive from PHP in Python3
+"""
+
+
+class CoinHiveAPI(object):
+
+    API_URL = 'https://api.coinhive.com'
+    secret = None
+
+    def __init__(self, secret):
+        if len(secret) != 32:
+            raise Exception('CoinHive - Invalid Secret')
+
+        self.secret = secret
+
+    def get(self, path, data={}):
+        data['secret'] = self.secret
+        url = self.API_URL + path + '?' + parse.urlencode(data)
+        response = requests.get(url)
+        return json.loads(response.text)
+
+    def post(self, path, data={}):
+        data['secret'] = self.secret
+        request = {'header': 'Content-type: application/x-www-form-urlencoded', 'content': parse.urlencode(data)}
+        url = self.API_URL + path
+        response = requests.post(url, request)
+        return json.loads(response.text)
